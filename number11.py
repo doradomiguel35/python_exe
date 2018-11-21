c20c112aa13669d0017d05914c755208a8d63dfd
Miguel Dorado <doradomiguel35@gmail.com>
2018-11-21 10:49:25 -0500

    not done

---
+++ number11.py
@@ -0,0 +1,6 @@
+def findObject(lists,property):
+	return property
+
+#Not yet Done
+mylist = list((dict(1='one',index=0),dict(2='two',index=1),dict(3='three',index=2)))
+print(findObject(mylist,{'index': 1}))
