9885b29d1e7d639a7b1fe9082c7444fcc90d2652
Miguel Dorado <doradomiguel35@gmail.com>
2018-11-22 11:53:33 +0800

    Done

--- number9.py
+++ number9.py
@@ -1,9 +1,11 @@
 def display_unique(lists):
-	for x in range(len(lists)):
-		for y in range(len(lists)):
-			if x == y:
-				print(str(x)+" "+str(y))
-				print(range(len(lists)))
-		lists.remove(lists[x])	
-#Not yet Done
-display_unique([1,2,3,1,3])
+	newList = []
+	for x in lists:
+		if x in lists:
+			newList.append(x)
+			lists.remove(x)
+	for x in lists:
+		if x in newList:
+			lists.remove(x)
+	return lists
+print(display_unique([1,2,3,1,3]))
