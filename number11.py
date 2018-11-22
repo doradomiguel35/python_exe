611a9d1b546a3b943bb427505509c361697ab96e
Miguel Dorado <doradomiguel35@gmail.com>
2018-11-22 11:32:33 +0800

    Not done

--- number11.py
+++ number11.py
@@ -1,7 +1,6 @@
 def find_Object(lists,properties):
-	result = []
-	for x in lists:
-		print(properties)
+	result = properties		
+	return result
 #Not yet Done
 mylist = [{1:'one','index':0},{2:'two','index':1},{3:'three','index': 2}]
 print(find_Object(mylist,{'index': 1}))
--- number9.py
+++ number9.py
@@ -1,8 +1,9 @@
-def display_unique(list):
-	for x in list:
-		if x in list:
-			list.pop(x)
-			break
-	print(list)
+def display_unique(lists):
+	for x in range(len(lists)):
+		for y in range(len(lists)):
+			if x == y:
+				print(str(x)+" "+str(y))
+				print(range(len(lists)))
+		lists.remove(lists[x])	
 #Not yet Done
 display_unique([1,2,3,1,3])
